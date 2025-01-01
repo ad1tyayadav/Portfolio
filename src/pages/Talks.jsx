8760b8a5aad5144138401457145779c9acba6f78
Adii Code <adityaproworks@gmail.com>
2025-01-01 10:51:07 +0530

    Fix Talk.js

--- src/pages/Talks.jsx
+++ src/pages/Talks.jsx
@@ -8,7 +8,7 @@ const Talks = () => {
     <>
       <Navbar />
       <section className="min-h-screen bg-black text-white py-24">
-        <div className="max-w-7xl mx-auto px-4 sm:px-6 w-[75%] lg:px-8">
+        <div className="max-w-7xl mx-auto sm:px-6 w-[75%] lg:px-8">
           <h2 className="text-4xl font-bold mb-4">Talks and Podcasts</h2>
           <p className="text-gray-400 mb-12">
             Explore insightful talks and podcasts about development, React, and beyond.
